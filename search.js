4fb5f67bc53f379130c1ad9a2117c08a64bad265
Morgan Achard <morgan4achard@gmail.com>
2025-02-14 16:59:09 +0100

    petit ajout

--- search.js
+++ search.js
@@ -6,6 +6,19 @@ recherche.addEventListener("click",()=>{
         alert("le champ est vide")
     }
     else{
-        alert("le champ n'est pas  vide")
-    }
+        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${champrecherche.value}`)
+            .then(response => {
+                if (!response.ok) {
+                throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log(data);
+                
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+        }
 })
