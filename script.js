0bc44a2c9d87600b6469beefb7411ebddc53bccd
Morgan Achard <morgan4achard@gmail.com>
2025-02-14 10:14:25 +0100

    random fait

---
+++ script.js
@@ -0,0 +1,39 @@
+
+const boutonPlat = document.querySelector('#PlatAleatoire')
+const NomPlat = document.querySelector('#NomPlat')
+const DetailPlat = document.querySelector('#DetailPlat')
+
+fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+        .then(response => {
+            if (!response.ok) {
+            throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+            
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+
+boutonPlat.addEventListener('click',()=>{
+    fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+        .then(response => {
+            if (!response.ok) {
+            throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+            NomPlat.textContent=data.meals[0].strMeal
+            DetailPlat.textContent=data.meals[0].strInstructions
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+
+})
+
