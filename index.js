91e4e29b186b2e5f6406ec069664877a808e2844
Morgan Achard <morgan4achard@gmail.com>
2025-02-19 16:52:26 +0100

    vraiment fini

--- index.js
+++ index.js
@@ -7,26 +7,27 @@ fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${para}`)
             return response.json();
         })
         .then(data => {
-            
-            if(data.meals[0].strMealThumb!=null){
-                const article=document.createElement('article');
-                const titre=document.createElement('h3');
-                const region=document.createElement('span');
-                const categorie=document.createElement('span');
-                const img=document.createElement('img');
-                const recette=document.querySelector("#platFavori");
-                titre.textContent=data.meals[0].strMeal;
-                region.textContent="Region : "+data.meals[0].strArea;
-                categorie.textContent="Categorie : " +data.meals[0].strCategory;
-                region.setAttribute("id","regionFav")
-                categorie.setAttribute("id","categorieFav")
-                img.setAttribute("src", data.meals[0].strMealThumb);
-                article.appendChild(titre);
-                article.appendChild(region);
-                article.appendChild(categorie);
-                article.appendChild(img);
-                recette.appendChild(article);
-            }
+            console.log(data)
+            const article=document.createElement('article');
+            const titre=document.createElement('h3');
+            const region=document.createElement('span');
+            const categorie=document.createElement('span');
+            const img=document.createElement('img');
+            const recette=document.querySelector("#platFavori");
+            titre.textContent=data.meals[0].strMeal;
+            region.textContent="Region : "+data.meals[0].strArea;
+            categorie.textContent="Categorie : " +data.meals[0].strCategory;
+            region.setAttribute("id","regionFav")
+            categorie.setAttribute("id","categorieFav")
+            img.setAttribute("src", data.meals[0].strMealThumb);
+            article.addEventListener("click", ()=>{
+                window.location.replace(`./meal.html?i=${data.meals[0].idMeal}`)
+            })
+            article.appendChild(titre);
+            article.appendChild(region);
+            article.appendChild(categorie);
+            article.appendChild(img);
+            recette.appendChild(article);
         })
         .catch(error => {
             console.error('Error:', error);
