078332bebe3a7b82dd4ed4b6fabe00e2fac7ff85
Morgan Achard <morgan4achard@gmail.com>
2025-02-21 09:44:30 +0100

    petite modif

--- search.js
+++ search.js
@@ -39,3 +39,44 @@ recherche.addEventListener("click",()=>{
             });
         }
 })
+let enter = document.querySelector("#champrecherche")
+enter.addEventListener('keydown', function(event) {
+  if (event.key === "Enter") {
+    if(champrecherche.value===""){
+        alert("Le champ est vide")
+    }
+    else{
+        listeRecherche.innerHTML = "";
+        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${champrecherche.value}`)
+            .then(response => {
+                if (!response.ok) {
+                throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                champrecherche.value=""
+                for(let i=0;i<data.meals.length;i++){
+                    if(data.meals[i].strMealThumb!=null){
+                        const article=document.createElement('article');
+                        const titre=document.createElement('h3');
+                        const img=document.createElement('img');
+                        const listeRecherche=document.querySelector("#listeRecherche");
+                        titre.textContent=data.meals[i].strMeal;
+                        img.setAttribute("src", data.meals[i].strMealThumb);
+                        article.appendChild(titre);
+                        article.appendChild(img);
+                        article.addEventListener("click", ()=>{
+                            window.location.replace(`./meal.html?i=${data.meals[i].idMeal}`)
+                        })
+                        listeRecherche.appendChild(article);
+                        
+                    }
+                } 
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+        }
+  }
+});
